b041adaaedaad89c5c6f805556bd948a2d5526b5
Sherwin Villaver <fdc-sherwinvillaver@gmail.com>
2018-04-11 11:52:38 +0800

    added playground and used the NSUserDefault

--- IOS-Training/IOS-Training/ViewController.swift
+++ IOS-Training/IOS-Training/ViewController.swift
@@ -8,17 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    //MARK: Properties
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var lblName: UILabel!
+    
+    
     override func viewDidLoad() {
+        if let name = UserDefaults.standard.value(forKey: "name") as? String {
+            lblName.text = name
+        }
+        txtName.delegate = self
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.tag == 1 {
+            lblName.text = textField.text!
+            UserDefaults.standard.set(textField.text!, forKey: "name")
+        }
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
 
 }
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var threePis = [Double](repeating: 3.14, count:3)
+var threeThaus = [Double](repeating: 6.283, count:3)
+threePis + threeThaus
