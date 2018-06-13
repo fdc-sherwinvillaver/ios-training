a7fb8daed6b77e8dc9d67e0ce4f85bae283c8aa8
Sherwin Villaver <fdc.sherwinvillaver@gmail.com>
2018-06-13 11:04:26 +0800

    Collection View Delegates

---
+++ IOS-Training/IOS-Training/FlickerPhotosViewController.swift
@@ -0,0 +1,110 @@
+//
+//  FlickerPhotosViewController.swift
+//  IOS-Training
+//
+//  Created by Lester  Padul on 13/06/2018.
+//  Copyright © 2018 Sherwin. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class FlickerPhotosViewController: UICollectionViewController {
+    //MARK: - Properties
+    
+    fileprivate let reuseIdentifier = "FlickrCell"
+    
+    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+    fileprivate var searches = [FlickrSearchResults]()
+    fileprivate let flickr = Flickr()
+    
+    fileprivate let itemsPerRow: CGFloat = 3
+}
+
+// MARK: - Private
+private extension FlickerPhotosViewController {
+    func photoForIndexPath(indexPath: IndexPath) -> FlickrPhoto {
+        return searches[(indexPath as NSIndexPath).section].searchResults[(indexPath as IndexPath).row]
+    }
+}
+
+extension FlickerPhotosViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        textField.addSubview(activityIndicator)
+        activityIndicator.frame = textField.bounds
+        activityIndicator.startAnimating()
+        
+        flickr.searchFlickrForTerm(textField.text!) {
+            results, error in
+            activityIndicator.removeFromSuperview()
+            
+            if let error = error {
+                print("Error searching: \(error)")
+                return
+            }
+            
+            if let results = results {
+                print("Found \(results.searchResults.count) matching \(results.searchTerm)")
+                self.searches.insert(results, at: 0)
+                
+                self.collectionView?.reloadData()
+            }
+        }
+        textField.text = nil
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension FlickerPhotosViewController {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return searches.count
+    }
+    
+    //2
+    override func collectionView(_ collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        return searches[section].searchResults.count
+    }
+    
+    //3
+    override func collectionView(_ collectionView: UICollectionView,
+                                 cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
+                                                      for: indexPath) as! FlickrPhotoCell
+        
+        let flickrPhoto = photoForIndexPath(indexPath: indexPath)
+        
+        cell.backgroundColor = UIColor.white
+        cell.imageView.image = flickrPhoto.thumbnail
+        
+        // Configure the cell
+        return cell
+    }
+}
+
+extension FlickerPhotosViewController : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth/itemsPerRow
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
+
+
+
+
+
+
---
+++ IOS-Training/IOS-Training/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  IOS-Training
+//
+//  Created by Lester  Padul on 13/06/2018.
+//  Copyright © 2018 Sherwin. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
